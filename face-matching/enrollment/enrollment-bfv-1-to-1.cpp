6bc70fb21afd391f28290b7dcb0f2cd0f93b849e
l_u_i <luis.unzueta@gmail.com>
2021-04-17 10:58:32 +0200

    Fix failures for VSCode in Windows

--- face-matching/enrollment/enrollment-bfv-1-to-1.cpp
+++ face-matching/enrollment/enrollment-bfv-1-to-1.cpp
@@ -33,9 +33,7 @@
 using namespace std;
 using namespace seal;
 
-int main()
-{
-
+int main() {
     float precision;
     stringstream stream;
     size_t poly_modulus_degree;
@@ -43,35 +41,41 @@ int main()
     precision = 125; // precision of 1/125 = 0.004
     poly_modulus_degree = 4096;
 
-    EncryptionParameters parms(scheme_type::BFV);
+    EncryptionParameters parms(scheme_type::bfv);
     parms.set_poly_modulus_degree(poly_modulus_degree);
     parms.set_coeff_modulus(CoeffModulus::BFVDefault(poly_modulus_degree));
-    parms.set_plain_modulus(PlainModulus::Batching(poly_modulus_degree, 20)); // seems like 16 also works
+    // seems like 16 also works
+    parms.set_plain_modulus(PlainModulus::Batching(poly_modulus_degree, 20));
 
     cout << "\nTotal memory allocated by global memory pool: "
-        << (MemoryPoolHandle::Global().alloc_byte_count() >> 20) << " MB" << endl;
+        << (MemoryPoolHandle::Global().alloc_byte_count() >> 20) << " MB"
+        << endl;
 
-    auto context = SEALContext::Create(parms);
+    auto context = make_shared<SEALContext>(parms);
     print_line(__LINE__);
     cout << "Set encryption parameters and print" << endl;
     print_parameters(context);
 
-    KeyGenerator keygen(context);
-    GaloisKeys gal_key = keygen.galois_keys();
-    RelinKeys relin_key = keygen.relin_keys();
-    PublicKey public_key = keygen.public_key();
+    KeyGenerator keygen(*context);
+    GaloisKeys gal_key;
+    keygen.create_galois_keys(gal_key);
+    RelinKeys relin_key;
+    keygen.create_relin_keys(relin_key);
+    PublicKey public_key;
+    keygen.create_public_key(public_key);
     SecretKey secret_key = keygen.secret_key();
 
-    Evaluator evaluator(context);
-    BatchEncoder batch_encoder(context);
-    Encryptor encryptor(context, public_key);
-    Decryptor decryptor(context, secret_key);
+    Evaluator evaluator(*context);
+    BatchEncoder batch_encoder(*context);
+    Encryptor encryptor(*context, public_key);
+    Decryptor decryptor(*context, secret_key);
 
     string name;
     ofstream ofile;
 
     // save the keys (public, secret, relin and galios)
-    name = "../data/keys/public_key_bfv_1_to_1.bin";
+    string data_path = "C:/LUI/code/secure-face-matching/data/";
+    name = data_path + "keys/public_key_bfv_1_to_1.bin";
     cout << "Saving Public Key: " << name << endl;
     ofile.open(name.c_str(), ios::out|ios::binary);
     public_key.save(stream);
@@ -79,7 +83,7 @@ int main()
     ofile.close();
     stream.str(std::string());
 
-    name = "../data/keys/secret_key_bfv_1_to_1.bin";
+    name = data_path + "keys/secret_key_bfv_1_to_1.bin";
     cout << "Saving Secret Key: " << name << endl;
     ofile.open(name.c_str(), ios::out|ios::binary);
     secret_key.save(stream);
@@ -87,7 +91,7 @@ int main()
     ofile.close();
     stream.str(std::string());
 
-    name = "../data/keys/relin_key_bfv_1_to_1.bin";
+    name = data_path + "keys/relin_key_bfv_1_to_1.bin";
     cout << "Saving Relin Keys: " <<  name << endl;
     ofile.open(name.c_str(), ios::out|ios::binary);
     relin_key.save(stream);
@@ -95,7 +99,7 @@ int main()
     ofile.close();
     stream.str(std::string());
 
-    name = "../data/keys/galios_key_bfv_1_to_1.bin";
+    name = data_path + "keys/galios_key_bfv_1_to_1.bin";
     cout << "Saving Galios Keys: " <<  name << endl;
     ofile.open(name.c_str(), ios::out|ios::binary);
     gal_key.save(stream);
@@ -106,30 +110,28 @@ int main()
 
     ifstream ifile;
     int num_gallery, dim_gallery;
-    ifile.open ("../data/gallery-1-to-1.bin", ios::in|ios::binary);
+    ifile.open (data_path + "gallery-1-to-1.bin", ios::in|ios::binary);
 
     ifile.read((char *)&num_gallery, sizeof(int));
     ifile.read((char *)&dim_gallery, sizeof(int));
 
     Plaintext plain_matrix;
-    float gallery[dim_gallery];
+    float* gallery = new float[dim_gallery];
     vector<int64_t> pod_matrix;
-    for (int i=0; i < num_gallery; i++)
-    {
+    for (int i=0; i < num_gallery; i++) {
         // Load gallery from file
         ifile.read((char *)gallery, dim_gallery * sizeof(float));
 
         // push gallery into a vector of size poly_modulus_degree
-        // actually we should be able to squeeze two gallery instances into one vector
-        // this depends on implementation, can get 2x speed up and 2x less storage
-        for (int j=0;j<slot_count / 2;j++)
-        {
-            if ((0 <= j) and (j < dim_gallery))
-            {
+        // actually we should be able to squeeze two gallery instances into one
+        // vector
+        // this depends on implementation, can get 2x speed up and 2x less
+        // storage
+        for (int j=0;j<slot_count / 2;j++) {
+            if ((0 <= j) && (j < dim_gallery)) {
                 int a = (int64_t) roundf(precision*gallery[j]);
                 pod_matrix.push_back(a);
-            }
-            else{
+            } else {
                 pod_matrix.push_back((int64_t) 0);
             }
         }
@@ -141,7 +143,8 @@ int main()
         encryptor.encrypt(plain_matrix, encrypted_matrix);
 
         // Save encrypted feature vector to disk.
-        name = "../data/gallery/encrypted_gallery_bfv_1_to_1_" + std::to_string(i) + ".bin";
+        name = data_path + "gallery/encrypted_gallery_bfv_1_to_1_" +
+            std::to_string(i) + ".bin";
         ofile.open(name.c_str(), ios::out|ios::binary);
         encrypted_matrix.save(stream);
         ofile << stream.str();
@@ -151,5 +154,6 @@ int main()
     }
     cout << "Done" << endl;
     ifile.close();
+    delete [] gallery;
     return 0;
 }
--- face-matching/enrollment/enrollment-bfv-1-to-n.cpp
+++ face-matching/enrollment/enrollment-bfv-1-to-n.cpp
@@ -33,9 +33,7 @@
 using namespace std;
 using namespace seal;
 
-int main()
-{
-
+int main() {
     float precision;
     stringstream stream;
     size_t poly_modulus_degree;
@@ -43,29 +41,33 @@ int main()
     precision = 125; // precision of 1/125 = 0.004
     poly_modulus_degree = 32768;
 
-    EncryptionParameters parms(scheme_type::BFV);
+    EncryptionParameters parms(scheme_type::bfv);
     parms.set_poly_modulus_degree(poly_modulus_degree);
     parms.set_coeff_modulus(CoeffModulus::BFVDefault(poly_modulus_degree));
-    parms.set_plain_modulus(PlainModulus::Batching(poly_modulus_degree, 20)); // seems like 16 also works
+    // seems like 16 also works
+    parms.set_plain_modulus(PlainModulus::Batching(poly_modulus_degree, 20));
 
     cout << "\nTotal memory allocated by global memory pool: "
         << (MemoryPoolHandle::Global().alloc_byte_count() >> 20) << " MB" << endl;
 
-    auto context = SEALContext::Create(parms);
+    auto context = make_shared<SEALContext>(parms);
     print_line(__LINE__);
     cout << "Set encryption parameters and print" << endl;
     print_parameters(context);
 
-    KeyGenerator keygen(context);
-    GaloisKeys gal_key = keygen.galois_keys();
-    RelinKeys relin_key = keygen.relin_keys();
-    PublicKey public_key = keygen.public_key();
+    KeyGenerator keygen(*context);
+    GaloisKeys gal_key;
+    keygen.create_galois_keys(gal_key);
+    RelinKeys relin_key;
+    keygen.create_relin_keys(relin_key);
+    PublicKey public_key;
+    keygen.create_public_key(public_key);
     SecretKey secret_key = keygen.secret_key();
 
-    Evaluator evaluator(context);
-    BatchEncoder batch_encoder(context);
-    Encryptor encryptor(context, public_key);
-    Decryptor decryptor(context, secret_key);
+    Evaluator evaluator(*context);
+    BatchEncoder batch_encoder(*context);
+    Encryptor encryptor(*context, public_key);
+    Decryptor decryptor(*context, secret_key);
 
     int slot_count = batch_encoder.slot_count();
     cout << "Plaintext matrix slot count: " << slot_count << endl;
@@ -74,7 +76,8 @@ int main()
     ofstream ofile;
 
     // save the keys (public, secret, relin and galios)
-    name = "../data/keys/public_key_bfv_1_to_n.bin";
+    string data_path = "C:/LUI/code/secure-face-matching/data/";
+    name = data_path + "keys/public_key_bfv_1_to_n.bin";
     cout << "Saving Public Key: " << name << endl;
     ofile.open(name.c_str(), ios::out|ios::binary);
     public_key.save(stream);
@@ -83,7 +86,7 @@ int main()
     stream.str(std::string());
     cout << "Done saving public keys" << endl;
 
-    name = "../data/keys/secret_key_bfv_1_to_n.bin";
+    name = data_path + "keys/secret_key_bfv_1_to_n.bin";
     cout << "Saving Secret Key: " << name << endl;
     ofile.open(name.c_str(), ios::out|ios::binary);
     secret_key.save(stream);
@@ -92,7 +95,7 @@ int main()
     stream.str(std::string());
     cout << "Done saving private keys" << endl;
 
-    name = "../data/keys/relin_key_bfv_1_to_n.bin";
+    name = data_path + "keys/relin_key_bfv_1_to_n.bin";
     cout << "Saving Relin Keys: " <<  name << endl;
     ofile.open(name.c_str(), ios::out|ios::binary);
     relin_key.save(stream);
@@ -103,14 +106,11 @@ int main()
 
 	ifstream ifile;
     int num_gallery, dim_gallery;
-    ifile.open ("../data/gallery-1-to-n.bin", ios::in|ios::binary);
+    ifile.open (data_path + "gallery-1-to-n.bin", ios::in|ios::binary);
 
-    if (ifile.fail())
-    {
+    if (ifile.fail()) {
       cout << name + " does not exist" << endl;
-    }
-    else
-    {
+    } else {
       ifile.read((char *)&dim_gallery, sizeof(int));
       ifile.read((char *)&num_gallery, sizeof(int));
     }
@@ -119,24 +119,20 @@ int main()
     cout << dim_gallery << endl;
 
     Plaintext plain_matrix;
-    float gallery[num_gallery];
+    float* gallery = new float[num_gallery];
     vector<int64_t> pod_matrix;
-    for (int i=0; i < dim_gallery; i++)
-    {
+    for (int i=0; i < dim_gallery; i++) {
         // Load gallery from file
         ifile.read((char *)&gallery, num_gallery * sizeof(float));
 
         // push dim i of all gallery into a vector of size poly_modulus_degree
         // assuming that gallery size is smaller than poly_modulus_degree
         // else need to chunk the gallery into blocks
-        for (int j=0;j<slot_count;j++)
-        {
-            if ((0 <= j) and (j < num_gallery))
-            {
+        for (int j=0;j<slot_count;j++) {
+            if ((0 <= j) && (j < num_gallery)) {
                 int a = (int64_t) roundf(precision*gallery[j]);
                 pod_matrix.push_back(a);
-            }
-            else{
+            } else {
                 pod_matrix.push_back((int64_t) 0);
             }
         }
@@ -148,7 +144,8 @@ int main()
         encryptor.encrypt(plain_matrix, encrypted_matrix);
 
         // Save encrypted feature vector to disk.
-        name = "../data/gallery/encrypted_gallery_bfv_1_to_n_" + std::to_string(i) + ".bin";
+        name = data_path + "gallery/encrypted_gallery_bfv_1_to_n_" +
+            std::to_string(i) + ".bin";
         ofile.open(name.c_str(), ios::out|ios::binary);
         encrypted_matrix.save(stream);
         ofile << stream.str();
@@ -158,5 +155,6 @@ int main()
     }
     cout << "Done" << endl;
     ifile.close();
+    delete [] gallery;
     return 0;
 }
